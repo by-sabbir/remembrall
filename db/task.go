e295809c1f1d1c21a06d64e2cedd43d1e646afd2
by-sabbir <me@sabbir.dev>
2023-06-10 17:41:57 +0600

    chore: sync on update

--- db/task.go
+++ db/task.go
@@ -10,11 +10,20 @@ import (
 
 func (d *DBClient) AddTask(ctx context.Context, t *v1.Task) (*v1.Task, error) {
 	tx := d.Client.Create(t)
-	info, err := d.Minio.FPutObject(ctx, "remembrall", "task.sqlite", "task.db", minio.PutObjectOptions{})
-	if err != nil {
-		log.Error("could not syncdb: ", err)
-	} else {
-		log.Info("synced db: ", info.Key)
+
+	errCh := make(chan error, 1)
+	go func() {
+		info, err := d.Minio.FPutObject(ctx, "remembrall", "task.sqlite", "task.db", minio.PutObjectOptions{})
+		if err != nil {
+			log.Error("could not syncdb: ", err)
+			errCh <- err
+		} else {
+			log.Info("synced db: ", info.Key)
+			errCh <- nil
+		}
+	}()
+	if <-errCh != nil {
+		log.Info("minio error: ", <-errCh)
 	}
 	return t, tx.Error
 }
@@ -30,6 +39,21 @@ func (d *DBClient) RemoveTask(ctx context.Context, id int) error {
 	var t v1.Task
 	t.ID = uint(id)
 	tx := d.Client.Delete(&t)
+
+	errCh := make(chan error, 1)
+	go func() {
+		info, err := d.Minio.FPutObject(ctx, "remembrall", "task.sqlite", "task.db", minio.PutObjectOptions{})
+		if err != nil {
+			log.Error("could not syncdb: ", err)
+			errCh <- err
+		} else {
+			log.Info("synced db: ", info.Key)
+			errCh <- nil
+		}
+	}()
+	if <-errCh != nil {
+		log.Info("minio error: ", <-errCh)
+	}
 
 	return tx.Error
 }
